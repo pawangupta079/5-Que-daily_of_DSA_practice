1c9903a3b26ecda34f8f7197ff114c84926945de
Pawan Kumar Gupta <133222848+pawangupta079@users.noreply.github.com>
2025-03-31 07:49:57 +0530

    Update generate_par.py

--- Day5/Easy/generate_par.py
+++ Day5/Easy/generate_par.py
@@ -1,13 +1,21 @@
-def generateParenthesis(n):  
-    def backtrack(s='', open=0, close=0):  
-        if len(s) == 2 * n:  
-            result.append(s)  
-            return  
-        if open < n:  
-            backtrack(s + '(', open + 1, close)  
-        if close < open:  
-            backtrack(s + ')', open, close + 1)  
 
-    result = []  
-    backtrack()  
-    return result  
+from typing import List
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        result = []
+
+        def backtrack(curr: str, open_count: int, close_count: int):
+            # Base Case: Valid sequence of length 2*n
+            if len(curr) == 2 * n:
+                result.append(curr)
+                return
+            # Add '(' if we have not used all open brackets
+            if open_count < n:
+                backtrack(curr + "(", open_count + 1, close_count)
+            # Add ')' if valid (should not exceed '(' count)
+            if close_count < open_count:
+                backtrack(curr + ")", open_count, close_count + 1)
+        backtrack("", 0, 0)
+        return result
+        
+
