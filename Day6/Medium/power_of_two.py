de5d766f499b4e2285a85cef072b945ed339fa8d
Pawan Kumar Gupta <133222848+pawangupta079@users.noreply.github.com>
2025-04-01 19:33:34 +0530

    power of two

---
+++ Day6/Medium/power_of_two.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    
+        for i in  range (32):
+            if n==2**i:
+                return "true"        
+            else:
+                pass
+        # or
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        return n&(n-1)==0 and n>0
