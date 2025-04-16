87bb557e589859eceeebb3f8f28529af81599143
Pawan Kumar Gupta <133222848+pawangupta079@users.noreply.github.com>
2025-04-16 20:29:48 +0530

    Update 13. Write a code to replace a substring in a string.cpp

--- 1__Top 50 Questions/13. Write a code to replace a substring in a string.cpp
+++ 1__Top 50 Questions/13. Write a code to replace a substring in a string.cpp
@@ -1,1 +1,17 @@
-#include <ios
+#include <iostream>
+using namespace std;
+int main() {
+    string str = "Hello, World!";
+    string toReplace = "World";
+    string replaceWith = "C++";
+
+    // Find the position of the substring to replace
+    size_t pos = str.find(toReplace);
+    if (pos != string::npos) {
+        // Replace the substring
+        str.replace(pos, toReplace.length(), replaceWith);
+    }
+
+    cout << str << endl; // Output: Hello, C++!
+    return 0;
+}
