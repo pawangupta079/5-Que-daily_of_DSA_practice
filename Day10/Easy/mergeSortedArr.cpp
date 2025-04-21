b490c8dcf83594d2260cc6f042ccd3c5bd9ba780
Pawan Kumar Gupta <133222848+pawangupta079@users.noreply.github.com>
2025-04-21 06:55:51 +0530

    added

---
+++ Day10/Easy/mergeSortedArr.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1;           // Pointer for nums1
+        int j = n - 1;           // Pointer for nums2
+        int k = m + n - 1;       // Pointer for merged position in nums1
+
+        // Merge in reverse order
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+
+        // If any elements left in nums2
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+
+        // No need to copy nums1's leftover values, they're already in place
+    }
+};
